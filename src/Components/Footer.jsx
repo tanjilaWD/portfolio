1843ae436f8f7ae2b64d15dbd26be31de63eda52
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-11-22 00:28:42 +0600

    feat:footer section

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='py-8 bg-[#3d3d3d] '>
+            <div className='container mx-auto px-6 text-center'>
+                <p className='text-gray-400'>© 2025 Tanjila Khatun. All Rights Reserved</p>
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default Footer;
